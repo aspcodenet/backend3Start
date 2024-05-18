48bd2d35fed5fd60308c1671b7d6b108193aff32
stefan <stefan.holmberg@systementor.se>
2024-05-18 08:58:31 +0200

    aaaa

--- build.gradle.kts
+++ build.gradle.kts
@@ -36,7 +36,7 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter-security")
     implementation("nz.net.ultraq.thymeleaf:thymeleaf-layout-dialect")
     implementation("org.thymeleaf.extras:thymeleaf-extras-springsecurity6")
-    implementation("org.springframework.security:spring-security-test")
+    implementation("org.springframework.boot:spring-boot-starter-oauth2-client")
     compileOnly("org.projectlombok:lombok")
     runtimeOnly("com.mysql:mysql-connector-j")
     annotationProcessor("org.projectlombok:lombok")
--- src/main/java/se/systementor/backend3start/security/WebSecurityConfig.java
+++ src/main/java/se/systementor/backend3start/security/WebSecurityConfig.java
@@ -2,18 +2,27 @@ package se.systementor.backend3start.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.authority.mapping.GrantedAuthoritiesMapper;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.oauth2.core.oidc.OidcIdToken;
+import org.springframework.security.oauth2.core.oidc.user.OidcUserAuthority;
+import org.springframework.security.oauth2.core.user.OAuth2UserAuthority;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.authentication.dao.*;
+
+import java.util.*;
 
 @Configuration
 @EnableWebSecurity
@@ -46,6 +55,11 @@ public class WebSecurityConfig {
                         .requestMatchers("/",  "/js/**", "/css/**", "/images/**", "/login/**", "/logout","/queues/**" ).permitAll()
                         .anyRequest().authenticated()
                 )
+                .oauth2Login(oauth2->{
+                    oauth2.userInfoEndpoint(ep->{
+                        ep.userAuthoritiesMapper( this.userAuthoritiesMapper() );
+                    });
+                })
                 .formLogin((form) -> form
 //                        .loginPage("/login")
                         .permitAll()
@@ -57,5 +71,40 @@ public class WebSecurityConfig {
                 .csrf(AbstractHttpConfigurer::disable);
 
         return http.build();
+    }
+
+    private GrantedAuthoritiesMapper userAuthoritiesMapper() {
+
+        return (authorities) -> {
+            List<SimpleGrantedAuthority> mappedAuthorities = new ArrayList<>();
+
+
+            authorities.forEach(authority -> {
+
+            if (authority instanceof OAuth2UserAuthority oauth2UserAuthority) {
+
+                Map<String, Object> userAttributes = oauth2UserAuthority.getAttributes();
+
+
+                //String email = userAttributes.get("email").toString();
+                // email is not returned from Github!!! If not public email setting is turned on in your account
+
+                // så - vi kan gå på login
+                String login = userAttributes.get("login").toString();
+
+                // Map the attributes found in userAttributes
+                // to one or more GrantedAuthority's and add it to mappedAuthorities
+                if(login.equals("aspcodenet")){
+                    mappedAuthorities.add(new SimpleGrantedAuthority("Admin"));
+                }
+            }
+
+            });
+
+
+
+
+            return mappedAuthorities;
+        };
     }
 }
